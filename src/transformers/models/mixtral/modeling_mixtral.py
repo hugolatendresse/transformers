0cec7408794d0493710dda06502c863f3d8e4c60
Hugo Latendresse <hugolatendresse@gmail.com>
2025-02-13 17:37:17 -0500

    flexflow naming

--- src/transformers/models/mixtral/modeling_mixtral.py
+++ src/transformers/models/mixtral/modeling_mixtral.py
@@ -636,42 +636,41 @@ class MixtralSparseMoeBlock(nn.Module):
             hidden_states *= torch.empty_like(hidden_states).uniform_(1.0 - self.jitter_noise, 1.0 + self.jitter_noise)
         hidden_states = hidden_states.view(-1, hidden_dim)
         # router_logits: (batch * sequence_length, n_experts)
-        router_logits = self.gate(hidden_states) # (batch x seq_len) x n_experts
+        gate = self.gate(hidden_states) # (batch x seq_len) x n_experts
 
-        routing_weights = F.softmax(router_logits, dim=1, dtype=torch.float) # (batch x seq_len) x n_experts
-        routing_weights, selected_experts = torch.topk(routing_weights, self.top_k, dim=-1)  # (batch x seq_len) x top_k weights, (batch x seq_len) x top_k indices
-        routing_weights /= routing_weights.sum(dim=-1, keepdim=True) # (batch x seq_len) x top_k normalized weights
-        # we cast back to the input dtype
-        routing_weights = routing_weights.to(hidden_states.dtype)  # (batch x seq_len) x top_k normalized weights
+        topk_values = F.softmax(gate, dim=1, dtype=torch.float) # (batch x seq_len) x n_experts
+        topk_values, topk_indices = torch.topk(topk_values, self.top_k, dim=-1)  # (batch x seq_len) x top_k weights, (batch x seq_len) x top_k indices
+        topk_values_reduced = topk_values.sum(dim=-1, keepdim=True)
+        topk_values = (topk_values / topk_values_reduced).to(hidden_states.dtype) # (batch x seq_len) x top_k normalized weights
 
-        final_hidden_states = torch.zeros(
+        mlp_out = torch.zeros(
             (batch_size * sequence_length, hidden_dim), dtype=hidden_states.dtype, device=hidden_states.device
         )
 
         # One hot encode the selected experts to create an expert mask
         # this will be used to easily index which expert is going to be sollicitated
-        expert_mask = torch.nn.functional.one_hot(selected_experts, num_classes=self.num_experts).permute(2, 1, 0) #  n_experts x top_k x (batch x seq_len)
+        expert_mask = torch.nn.functional.one_hot(topk_indices, num_classes=self.num_experts).permute(2, 1, 0) #  n_experts x top_k x (batch x seq_len)
 
         # Loop over all available experts in the model and perform the computation on each expert
         for expert_idx in range(self.num_experts):
-            expert_layer = self.experts[expert_idx]
-            idx, top_x = torch.where(expert_mask[expert_idx])  # MixtralBlockSparseTop2MLP (just a 2-layer MLP)
+            expert_layer = self.experts[expert_idx]  # MixtralBlockSparseTop2MLP (just a 2-layer MLP)
+            idx, top_x = torch.where(expert_mask[expert_idx])
             # expert_mask[expert_idx] is top_k x (batch x seq_len)
             # idx is a 1D list of the row index (out of top_k)
             # top_k is a 1D list of the column index (token index out of (batch x seq_len)) that have the expert
 
-
             # Index the correct hidden states and compute the expert hidden state for
             # the current expert. We need to make sure to multiply the output hidden
-            # states by `routing_weights` on the corresponding tokens (top-1 and top-2)
-            current_state = hidden_states[None, top_x].reshape(-1, hidden_dim)# (number of tokens using this expert) x hidden_dim
-            current_hidden_states = expert_layer(current_state) * routing_weights[top_x, idx, None] # (number of tokens using this expert) x hidden_dim
+            # states by `topk_values` on the corresponding tokens (top-1 and top-2)
+            grouped_tokens_expert_idx = hidden_states[None, top_x].reshape(-1, hidden_dim)# (number of tokens using this expert) x hidden_dim
+
+            current_hidden_states = expert_layer(grouped_tokens_expert_idx) * topk_values[top_x, idx, None] # (number of tokens using this expert) x hidden_dim
 
             # However `index_add_` only support torch tensors for indexing so we'll use
             # the `top_x` tensor here.
-            final_hidden_states.index_add_(0, top_x, current_hidden_states.to(hidden_states.dtype))  # (batch_size x sequence_length) x hidden_dim
-        final_hidden_states = final_hidden_states.reshape(batch_size, sequence_length, hidden_dim)
-        return final_hidden_states, router_logits # batch_size x sequence_length x hidden_dim,   (batch x seq_len) x n_experts
+            mlp_out.index_add_(0, top_x, current_hidden_states.to(hidden_states.dtype))  # (batch_size x sequence_length) x hidden_dim
+        mlp_out = mlp_out.reshape(batch_size, sequence_length, hidden_dim)
+        return mlp_out, gate # batch_size x sequence_length x hidden_dim,   (batch x seq_len) x n_experts
 
 
 class MixtralDecoderLayer(nn.Module):
